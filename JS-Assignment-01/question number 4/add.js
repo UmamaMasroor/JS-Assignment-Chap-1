4e5ad514188453a011c197cd783d5abe7bdfb0fa
Umama Masroor <umamafatima319@gmail.com>
2023-04-09 17:02:35 -0700

    question number 4

---
+++ JS-Assignment-01/question number 4/add.js
@@ -0,0 +1,4 @@
+// 4. Write a script to display following messages in sequence
+
+alert("JavaScript Alert \n\n Welcome to JS land..");
+alert("JavaScript Alert \n\n Happy Coding! \n Prevent this page from creating additional dialogs")
